9ad8cd413362806470d423048918f10ff75a9580
uniocre <1205575231@qq.com>
2019-05-05 17:58:39 +0800

    支付页添加地址功能（未完成）

--- src/views/shop/pay.vue
+++ src/views/shop/pay.vue
@@ -17,54 +17,148 @@
       </div>
       <!-- 拍下商品 -->
       <div v-show="active === 0">
-        <div class="addres">
-          <div class="address-top clearfix">
-            <p style="float:left;">确认收货地址</p>
-            <div style="float:right;"
-                 @click="goedit">管理收货地址</div>
+        <div class="addres clearfic">
+          <div class="address-top">
+            <p>确认收货地址</p>
           </div>
           <!-- 收货地址选择 -->
-          <p>寄送至:</p>
-          <ul v-for="(value,index) in address"
-              :key="index"
-              class="payaddress">
-            <li>{{value.address}} ({{value.name}} 收) {{value.phone}}</li>
-          </ul>
+          <div class="shouhuo">
+            <p style="margin-top:20px;">寄送至:</p>
+            <ul v-for="(value,index) in address"
+                :key="index"
+                class="payaddress">
+              <li>{{value.address}} ({{value.name}} 收) {{value.phone}}</li>
+            </ul>
+            <!-- 添加收货地址 -->
+
+            <el-button type="text"
+                       @click="dialogVisible = true">添加收货地址</el-button>
+
+            <el-dialog title="提示"
+                       :visible.sync="dialogVisible"
+                       width="80%"
+                       :before-close="handleClose">
+              <el-form :model="ruleForm"
+                       :rules="rules"
+                       ref="ruleForm"
+                       label-width="100px"
+                       class="demo-ruleForm">
+                <el-form-item label="地址信息"
+                              prop="name">
+                  <el-cascader :options="options"
+                               v-model="selectedOptions"></el-cascader>
+                </el-form-item>
+                <el-form-item label="活动区域"
+                              prop="region">
+                  <el-select v-model="ruleForm.region"
+                             placeholder="请选择活动区域">
+                    <el-option label="区域一"
+                               value="shanghai"></el-option>
+                    <el-option label="区域二"
+                               value="beijing"></el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="活动时间"
+                              required>
+                  <el-col :span="11">
+                    <el-form-item prop="date1">
+                      <el-date-picker type="date"
+                                      placeholder="选择日期"
+                                      v-model="ruleForm.date1"
+                                      style="width: 100%;"></el-date-picker>
+                    </el-form-item>
+                  </el-col>
+                  <el-col class="line"
+                          :span="2">-</el-col>
+                  <el-col :span="11">
+                    <el-form-item prop="date2">
+                      <el-time-picker placeholder="选择时间"
+                                      v-model="ruleForm.date2"
+                                      style="width: 100%;"></el-time-picker>
+                    </el-form-item>
+                  </el-col>
+                </el-form-item>
+                <el-form-item label="即时配送"
+                              prop="delivery">
+                  <el-switch v-model="ruleForm.delivery"></el-switch>
+                </el-form-item>
+                <el-form-item label="活动性质"
+                              prop="type">
+                  <el-checkbox-group v-model="ruleForm.type">
+                    <el-checkbox label="美食/餐厅线上活动"
+                                 name="type"></el-checkbox>
+                    <el-checkbox label="地推活动"
+                                 name="type"></el-checkbox>
+                    <el-checkbox label="线下主题活动"
+                                 name="type"></el-checkbox>
+                    <el-checkbox label="单纯品牌曝光"
+                                 name="type"></el-checkbox>
+                  </el-checkbox-group>
+                </el-form-item>
+                <el-form-item label="特殊资源"
+                              prop="resource">
+                  <el-radio-group v-model="ruleForm.resource">
+                    <el-radio label="线上品牌商赞助"></el-radio>
+                    <el-radio label="线下场地免费"></el-radio>
+                  </el-radio-group>
+                </el-form-item>
+                <el-form-item label="活动形式"
+                              prop="desc">
+                  <el-input type="textarea"
+                            v-model="ruleForm.desc"></el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary"
+                             @click="submitForm('ruleForm')">立即创建</el-button>
+                  <el-button @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+              </el-form>
+              <span slot="footer"
+                    class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary"
+                           @click="dialogVisible = false">保存</el-button>
+              </span>
+            </el-dialog>
+          </div>
           <!-- 确认订单信息 -->
-          <p style="margin:10px 0;">确认订单信息</p>
+          <p style="margin:20px 0;">确认订单信息</p>
           <!-- 商品 -->
-          <el-table :data="tableData"
-                    style="width: 100%">
-            <el-table-column label="商品">
-              <template slot-scope="scope">
-                <img :src="scope.row.img"
-                     alt=""
-                     style="height:30px;width:50px;">
-                <span style="margin-left: 10px">{{ scope.row.name }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="单价"
-                             prop="UnitPrice">
-            </el-table-column>
-            <el-table-column label="数量"
-                             prop="number">
-              <template slot-scope="scope">
-                <el-input-number size="mini"
-                                 v-model="scope.row.number"
-                                 :min="1"
-                                 @change="handleChange(scope.row)"></el-input-number>
-              </template>
-            </el-table-column>
-            <el-table-column label="金额"
-                             prop="totalPrice">
-            </el-table-column>
-          </el-table>
-          <div class="div">
-            {{calculatePrice}}
+          <div class="mail">
+            <el-table :data="tableData"
+                      style="width: 100%">
+              <el-table-column label="商品">
+                <template slot-scope="scope">
+                  <img :src="scope.row.img"
+                       alt=""
+                       style="height:30px;width:50px;">
+                  <span style="margin-left: 10px">{{ scope.row.name }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="单价"
+                               prop="UnitPrice">
+              </el-table-column>
+              <el-table-column label="数量"
+                               prop="number">
+                <template slot-scope="scope">
+                  <el-input-number size="mini"
+                                   v-model="scope.row.number"
+                                   :min="1"
+                                   @change="handleChange(scope.row)"></el-input-number>
+                </template>
+              </el-table-column>
+              <el-table-column label="金额"
+                               prop="totalPrice">
+              </el-table-column>
+            </el-table>
+            <div class="div">
+              金额为:￥<p style="color:red;display:inline-block;">{{calculatePrice}}</p>
+            </div>
           </div>
         </div>
       </div>
-      <el-button style="margin-top: 12px"
+      <el-button style="margin-top: 20px;
+      float:right"
                  @click="next">确认订单</el-button>
     </div>
   </div>
@@ -78,6 +172,47 @@ export default {
   },
   data () {
     return {
+      /* 管理收货地址弹框 */
+      dialogVisible: false,
+      /* 地区选择 */
+      options: [],
+      /* 收货信息表单 */
+      ruleForm: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      /* 规则验证 */
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        region: [
+          { required: true, message: '请选择活动区域', trigger: 'change' }
+        ],
+        date1: [
+          { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+        ],
+        date2: [
+          { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+        ],
+        type: [
+          { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+        ],
+        resource: [
+          { required: true, message: '请选择活动资源', trigger: 'change' }
+        ],
+        desc: [
+          { required: true, message: '请填写活动形式', trigger: 'blur' }
+        ]
+      },
+      /* 步骤条 */
       active: 0,
       /* 全部商品总价 */
       Price: 0,
@@ -147,6 +282,14 @@ export default {
     this.Lprice()
   },
   methods: {
+    /* 管理收货地址 */
+    handleClose (done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => { })
+    },
     goedit () { },
     /* 商品金额 */
     Lprice () {
@@ -180,11 +323,20 @@ export default {
   width: 1200px;
   margin: 20px auto;
 }
+.addres {
+  margin-top: 20px;
+}
+/* button */
+.div {
+  margin: 20px 0 10px 1098px;
+}
 /* 地址 */
 .payaddress {
   li {
     text-indent: 12px;
     margin: 5px auto;
+    height: 30px;
+    line-height: 30px;
   }
 }
 </style>
